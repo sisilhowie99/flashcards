032bcbef6d6ded56090758538e4aacce1d222a99
sisilhowie99 <sisil.anastasia@gmail.com>
2022-07-29 22:35:01 +1000

    Add quizzes slice to store
    * Use selector to get quizzes slice in Quizzes and render them
    * Use selector to get quizzes slice to Quiz render selected quiz's data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import topicsReducer from '../features/topics/TopicsSlice';
+import quizzesReducer from '../features/quizzes/QuizzesSlice';
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
+    quizzes: quizzesReducer,
   },
 });
